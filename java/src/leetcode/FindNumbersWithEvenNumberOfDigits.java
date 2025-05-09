81ea3dfd93391884099051391a5cbdaf92e854ea
leejongnyeong <ffeer0508@accurasoft.co.kr>
2025-05-09 17:00:53 +0900

    array

---
+++ java/src/leetcode/FindNumbersWithEvenNumberOfDigits.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class FindNumbersWithEvenNumberOfDigits {
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        for(int num : nums) {
+            if(String.valueOf(num).length() % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
---
+++ java/src/leetcode/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+public class RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int i = 0; // 고유 원소가 들어갈 위치
+
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        RemoveDuplicatesFromSortedArray removeDuplicatesFromSortedArray = new RemoveDuplicatesFromSortedArray();
+        int[] a = {1,1,2};
+        int i = removeDuplicatesFromSortedArray.removeDuplicates(a);
+        System.out.println("i = " + i);
+    }
+}
