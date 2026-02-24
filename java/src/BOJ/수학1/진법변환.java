7fc4788da41ed02c16585f6ff6de4545334ecdcb
nyeong-nyeong <ffeer0508@gmail.com>
2026-02-25 01:40:43 +0900

    02/25_BOJ class명 변경

--- java/src/BOJ/수학1/진법변환.java
+++ java/src/BOJ/수학1/진법변환.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class 진번변환 {
+public class 진법변환 {
     public static void main(String[] args) throws IOException {
         // 2745
 
