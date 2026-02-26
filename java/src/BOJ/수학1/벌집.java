0979e14ada56470a67335ce099c9ceff6c4d24f7
leejongnyeong <ffeer0508@accurasoft.co.kr>
2026-02-26 13:04:41 +0900

    2/26_BOJ 벌집 번호 추가

--- java/src/BOJ/수학1/벌집.java
+++ java/src/BOJ/수학1/벌집.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 
 public class 벌집 {
     public static void main(String[] args) throws IOException {
-
+        // 2292
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int target = Integer.parseInt(br.readLine());
 
